2a2b92d5713aa26e6bf685e0149d86485087e85c
Tom Forbes <tom@tomforb.es>
2020-07-04 09:41:16 +0000

    {"name":"imgyaso","version":"2020.7.4","file":"imgyaso-2020.7.4.tar.gz","path":"imgyaso/imgyaso-2020.7.4.tar.gz"}

---
+++ imgyaso/imgyaso-2020.7.4.tar.gz/imgyaso/quant.py
@@ -0,0 +1,47 @@
+import sys
+import cv2
+import libimagequant as liq
+from PIL import Image
+from io import BytesIO
+from .util import *
+
+def pngquant(img, ncolors=8):
+    img = bytes(cv2.imencode('.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 9])[1])
+    img = pngquant_bts(img, ncolors)
+    return cv2.imdecode(np.frombuffer(img, np.uint8), cv2.IMREAD_UNCHANGED)
+
+def pngquant_bts(img, ncolors=8):
+    img = conv2png(img)
+    img = Image.open(BytesIO(img)).convert('RGBA')
+    w, h = img.width, img.height
+    bytes = img.tobytes()
+    
+    attr = liq.Attr()
+    attr.max_colors = ncolors
+    img = attr.create_rgba(bytes, w, h, 0)
+    res = img.quantize(attr)
+    res.dithering_level = 1.0
+    bytes = res.remap_image(img)
+    palette = res.get_palette()
+    
+    img = Image.frombytes('P', (w, h), bytes)
+    
+    palette_data = []
+    for color in palette:
+        palette_data.append(color.r)
+        palette_data.append(color.g)
+        palette_data.append(color.b)
+    img.putpalette(palette_data)
+    
+    bio = BytesIO()
+    img.save(bio, 'PNG', optimize=True)
+    return bio.getvalue()
+
+def main():
+    fname = sys.argv[1]
+    img = open(fname, 'rb').read()
+    img = pngquant_bts(img)
+    with open(fname, 'wb') as f:
+        f.write(img)
+    
+if __name__ == '__main__': main()
